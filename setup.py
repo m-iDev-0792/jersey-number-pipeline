61ef2e925af51c8a205f722f563b35b3a3f2e38d
m-iDev-0792 <1015732477@qq.com>
2025-01-30 01:17:08 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ def setup_str(root):
     os.chdir(os.path.join(rep_path, repo_name))
 
     if not env_name in get_conda_envs():
-        make_conda_env(env_name, libs="python=3.11")
+        make_conda_env(env_name, libs="python=3.9")
         cuda_ver = 'cu118'
         os.system(f"make torch-{cuda_ver}")
         os.system(f"conda run --live-stream -n {env_name} conda install --name {env_name} pip")
