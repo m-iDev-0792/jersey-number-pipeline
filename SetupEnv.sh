25db1807fb43194d4dea9b543b47709aa1d82d09
m-iDev-0792 <1015732477@qq.com>
2025-01-30 02:07:45 -0800

    Update SetupEnv.sh

--- SetupEnv.sh
+++ SetupEnv.sh
@@ -3,3 +3,4 @@ conda activate SoccerNet
 pip install gdown
 pip install tqdm
 pip install SoccerNet
+conda install icrawler matplotlib skimage scikit-image h5py pandas opencv-python docwriter tqdm joblib dominate opencv-contrib-python mpl_toolkit mpl-tools pywin32 viola gdown https numpy Pillow scipy yacs einops
