0f15c71e06813407b6c180143198d7831d40fbc1
m-iDev-0792 <1015732477@qq.com>
2025-02-07 13:08:38 +0800

    compatible with Windows

---
+++ SetupEnv.bat
@@ -0,0 +1,10 @@
+echo y | conda create --name SoccerNet python=3.9
+conda activate SoccerNet
+pip install gdown
+pip install tqdm
+pip install SoccerNet
+pip install pandas
+pip install opencv-python
+pip3 install torch==1.10.0+cu113 torchvision==0.11.1+cu113 torchaudio==0.10.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+python SetupSoccerNetDataset.py
+python setup.py SoccerNet
--- setup.py
+++ setup.py
@@ -104,6 +104,7 @@ def setup_pose(root):
         os.system(f"conda run --live-stream -n {env_name} pip install -v -e .")
         os.system(f"conda run --live-stream -n {env_name} pip install tqdm")
         os.system(f"conda run --live-stream -n {env_name} pip install timm==0.4.9 einops")
+        os.system(f'conda run --live-stream -n {env_name} pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html')
     print(f'setup_pose(): ======================= end =======================')
 
 # clone and install str
